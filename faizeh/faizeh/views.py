fbb3c816dc11838e2c5f26f2212f3ed453916f62
faizahkuratulainie <faizahkurratulaini@gmail.com>
2022-07-22 19:57:00 +0700

    uas

---
+++ faizeh/faizeh/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index (request):
+    context = {
+        'judul':'home'
+}
+    return render(request,'index.html',context)
